46b4615cfac2bfaa11bde70d8c5575ac9f2b7622
amin <aminalaee4@gmail.com>
2024-07-18 17:21:34 +0330

    add

---
+++ PhotoSearch/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.css'
+import App from './Component/App';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
+        <App />
+);
